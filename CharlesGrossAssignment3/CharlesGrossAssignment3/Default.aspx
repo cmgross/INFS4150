212a6d9989237aac1aa5ed835d5f2af04c2caf2e
Uziel <chuckgross@gmail.com>
2014-02-27 20:49:10 -0500

    Added assignment 3

---
+++ CharlesGrossAssignment3/CharlesGrossAssignment3/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="CharlesGrossAssignment3._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="width: 100%;">
+        <tr>
+            <td>&nbsp;</td>
+            <td><asp:DropDownList ID="ddlUsers" runat="server" DataSourceID="sdsUsers" DataTextField="CName" DataValueField="ID" AutoPostBack="True"></asp:DropDownList>
+                <asp:SqlDataSource ID="sdsUsers" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [userchar]"></asp:SqlDataSource>
+            </td>
+            <td rowspan="4"><asp:Image ID="imgIcon" runat="server" /></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>Gold<asp:Literal ID="lGold" runat="server"></asp:Literal></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>XP<asp:Literal ID="lExp" runat="server"></asp:Literal></td>
+        </tr>
+         <tr>
+            <td>&nbsp;</td>
+            <td>
+                <asp:Button ID="btnSelectCharacter" runat="server" Text="Select Character" CssClass="btn btn-primary" /></td>
+        </tr>
+    </table>
+
+</asp:Content>
---
+++ CharlesGrossAssignment3/CharlesGrossAssignment3/Default.aspx.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Data
+
+Public Class _Default
+    Inherits Page
+
+    Public Class GameUser
+        Public Property Id As Integer
+        Public Property Name As String
+        Public Property Rloc As Integer
+        Public Property Cloc As Integer
+        Public Property Gold As Integer
+        Public Property Health As Integer
+        Public Property Exp As Integer
+        Public Property Icon As String
+    End Class
+
+    Private _selectedUser As GameUser
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        'TODO switch to SelectedIndexChange method
+        If Not IsPostBack Then ddlUsers.DataBind()
+        _selectedUser = GetSelectedUser()
+        lGold.Text = _selectedUser.Gold
+        lExp.Text = _selectedUser.Exp
+        imgIcon.ImageUrl = "Images/" + _selectedUser.Icon 'TODO find icons for characters as same size, and put in db and images folder
+    End Sub
+
+    Private Function GetSelectedUser() As GameUser 'TODO refactor this ugh
+        Dim uid As String = "ID =" & ddlUsers.SelectedValue
+        Dim userTable As DataView = CType(sdsUsers.Select(DataSourceSelectArguments.Empty), DataView)
+        userTable.RowFilter = uid
+        Dim userRow As DataRowView = CType(userTable(0), DataRowView)
+        Dim gameUser As New GameUser
+        gameUser.Id = userRow("ID")
+        gameUser.Name = userRow("CName")
+        gameUser.Rloc = userRow("rloc")
+        gameUser.Cloc = userRow("cloc")
+        gameUser.Gold = userRow("gold")
+        gameUser.Exp = userRow("exp")
+        gameUser.Health = userRow("health")
+        gameUser.Icon = userRow("icon")
+        Return gameUser
+    End Function
+
+    Protected Sub btnSelectCharacter_Click(sender As Object, e As EventArgs) Handles btnSelectCharacter.Click
+        'TODO Load control into session variables
+        'TODO Use bootswatch - superhero
+    End Sub
+End Class
